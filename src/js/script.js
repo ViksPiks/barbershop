d92bb273427f6852bc4ab4d569e48eb56d64ee4a
viks <victoria.miledina@gmail.com>
2023-08-10 23:53:30 +0300

    feat: add map modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
+// Mobile menu
 const menu = document.querySelector(".header__nav");
 const body = document.body;
 
@@ -8,6 +9,7 @@ hamburger.addEventListener("click", () => {
   menu.classList.toggle("header__nav--open");
 });
 
+// Login Modal
 const loginBtn = document.getElementById("login-btn");
 const modalLogin = document.getElementById("modal-login");
 
@@ -27,5 +29,28 @@ modalLogin.addEventListener("click", (event) => {
     rect.bottom < event.clientY
   ) {
     modalLogin.close();
+  }
+});
+
+// Map Modal
+const findUsBtn = document.getElementById("find-us");
+const modalMap = document.getElementById("modal-map");
+
+findUsBtn.addEventListener("click", () => {
+  modalMap.showModal();
+});
+
+modalMap.addEventListener("click", (event) => {
+  if (event.target !== modalMap) return;
+
+  const rect = event.target.getBoundingClientRect();
+
+  if (
+    rect.left > event.clientX ||
+    rect.right < event.clientX ||
+    rect.top > event.clientY ||
+    rect.bottom < event.clientY
+  ) {
+    modalMap.close();
   }
 });
