27eb706fe1b5980b6b818f77c0a5709d978e22db
viks <victoria.miledina@gmail.com>
2023-08-10 23:24:09 +0300

    fix: fix responsive for login dialog

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,31 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".header__nav");
+const body = document.body;
 
 hamburger.addEventListener("click", () => {
+  body.classList.toggle("disable-scroll");
   hamburger.classList.toggle("hamburger--open");
   menu.classList.toggle("header__nav--open");
+});
+
+const loginBtn = document.getElementById("login-btn");
+const modalLogin = document.getElementById("modal-login");
+
+loginBtn.addEventListener("click", () => {
+  modalLogin.showModal();
+});
+
+modalLogin.addEventListener("click", (event) => {
+  if (event.target !== modalLogin) return;
+
+  const rect = event.target.getBoundingClientRect();
+
+  if (
+    rect.left > event.clientX ||
+    rect.right < event.clientX ||
+    rect.top > event.clientY ||
+    rect.bottom < event.clientY
+  ) {
+    modalLogin.close();
+  }
 });
