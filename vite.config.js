7b3cbeb831488a6bf784599aa4d698b835ee1042
viks <victoria.miledina@gmail.com>
2023-08-12 21:25:53 +0300

    fix: fix multipage apps building

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import { resolve } from "path";
+
 export default {
   root: "./src",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        catalog: resolve(__dirname, "src/catalog.html"),
+      },
+    },
   },
 };
